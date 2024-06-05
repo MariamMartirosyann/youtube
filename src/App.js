c76703753d42dfa2ffb22a83593653bff8dab024
mariammartirosyan16 <mariammartirosyan1988@mail.ru>
2024-06-06 00:07:07 +0400

    add: Shimmer UI

--- src/App.js
+++ src/App.js
@@ -1,32 +1,51 @@
-import {  RouterProvider, createBrowserRouter } from "react-router-dom";
+import React, { lazy, Suspense } from "react";
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
 import { Provider } from "react-redux";
 import Body from "./components/Body";
 import store from "./utils/store";
 import "./App.css";
-import MainContainer from "./components/MainContainer";
-import WatchPage from "./components/WatchPage";
-import SearchVideoList from "./components/SearchVideoList";
+import ShimmerList from "./components/ShimmerList";
 
+const MainContainer = lazy(() => import("./components/MainContainer"));
+const WatchPage = lazy(() => import("./components/WatchPage"));
+const SearchVideoList = lazy(() => import("./components/SearchVideoList"));
 
 const appRouter = createBrowserRouter([
   {
     path: "/",
     element: <Body />,
     children: [
-      { path: "/", element: <MainContainer /> },
-      { path: "watch", element: <WatchPage /> },
-      { path:"results", element:<SearchVideoList/>}
+      {
+        path: "/",
+        element: (
+          <Suspense fallback={<ShimmerList/>}>
+            <MainContainer />
+          </Suspense>
+        ),
+      },
+      {
+        path: "watch",
+        element: (
+          <Suspense fallback={<h1>Loading....</h1>}>
+            <WatchPage />
+          </Suspense>
+        ),
+      },
+      {
+        path: "results",
+        element: (
+          <Suspense fallback={<h1>Loading....</h1>}>
+            <SearchVideoList />
+          </Suspense>
+        ),
+      },
     ],
   },
 ]);
 function App() {
   return (
     <Provider store={store}>
-      <div>
-        {/* <Head /> */}
-        <RouterProvider router={appRouter} />
-        
-      </div>
+      <RouterProvider router={appRouter} />
     </Provider>
   );
 }
